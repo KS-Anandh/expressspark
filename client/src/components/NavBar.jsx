e795f94901536686c4c8bf939d0215578b938705
KS-Anandh <167880483+KS-Anandh@users.noreply.github.com>
2025-02-18 11:47:42 +0530

    Update NavBar.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,11 +10,11 @@ const NavBar = ({navigation,SetNavigation}) => {
         <div className="list">
           <ul>
            <Link to={'/'}><li style={{color:navigation==0?"orangered":"white"}} onClick={()=>SetNavigation(0)}>Home</li></Link>
-           <Link to={'/afterSchoolProgram'}><li style={{color:navigation==1?"orangered":"white"}} onClick={()=>SetNavigation(1)}>After Schooling Program</li></Link>
-            <Link to={'/aboutUs'}><li style={{color:navigation==4?"orangered":"white"}} onClick={()=>SetNavigation(4)}>Company</li></Link>
-            <Link to={'/contactUs'}><li style={{color:navigation==5?"orangered":"white"} } onClick={()=>SetNavigation(5)}>Contact Us</li></Link>
             <Link to={'/forSchool'}><li style={{color:navigation==2?"orangered":"white"}} onClick={()=>SetNavigation(2)}>For Schools</li></Link>
+           <Link to={'/afterSchoolProgram'}><li style={{color:navigation==1?"orangered":"white"}} onClick={()=>SetNavigation(1)}>After Schooling Program</li></Link>
+            <Link to={'/contactUs'}><li style={{color:navigation==5?"orangered":"white"} } onClick={()=>SetNavigation(5)}>Contact Us</li></Link> 
             <li><a href="https://theexpresssparkacademy.com/" target='_blank'>Test Series</a></li>
+            <Link to={'/aboutUs'}><li style={{color:navigation==4?"orangered":"white"}} onClick={()=>SetNavigation(4)}>Company</li></Link>   
           </ul>
         </div>
     </div>
